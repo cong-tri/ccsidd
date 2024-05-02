28483d9917c4c1cf4791386b7e2357c00412be90
Dao Cong Tri <congtri16092003@gmail.com>
2024-05-02 14:29:17 +0700

    upgrade contact page

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -1,15 +1,28 @@
 /** @format */
 import React from "react";
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 import { Row, Col } from "antd";
 import Title from "antd/es/typography/Title";
 import FormContact from "./_components/FormContact";
 import MapContact from "./_components/MapContact";
 import styles from "../../index.module.scss";
+const transporter = nodemailer.createTransport({
+  pool: true,
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
+  secure: true,
+  auth: {
+    type: "login",
+    user: process.env.SENDER_AUTH_USER,
+    pass: process.env.SENDER_AUTH_PASS,
+  },
+} as SMTPTransport.Options);
 const ContactPage = () => {
   const handleSendMail = async (values: any) => {
     "use server";
     try {
+      /*
       const transporter = nodemailer.createTransport({
         pool: true,
         host: process.env.SERVER_MAIL_HOST,
@@ -22,8 +35,8 @@ const ContactPage = () => {
         },
       });
       const mailOptions = {
-        from: process.env.SENDER_AUTH_USER,
-        to: process.env.SENDER_AUTH_USER,
+        from: process.env.SENDER_MAIL,
+        to: process.env.SENDER_MAIL,
         subject: "test send email",
         text: `
           Username: ${values.username}\n
@@ -32,7 +45,18 @@ const ContactPage = () => {
           Message: ${values.message}\n
         `,
       };
-      const info = await transporter.sendMail(mailOptions);
+      */
+      await transporter.sendMail({
+        from: process.env.SENDER_MAIL,
+        to: process.env.SENDER_MAIL,
+        subject: "test send email",
+        text: `
+          Username: ${values.username}\n
+          Email: ${values.email}\n
+          Kind Contact: ${values.kind}\n
+          Message: ${values.message}\n
+        `,
+      });
       transporter.verify(function (error, success) {
         if (error) {
           console.log(error);
@@ -40,7 +64,7 @@ const ContactPage = () => {
           console.log(success);
         }
       });
-      console.log(info.response);
+      transporter.close();
     } catch (error) {
       console.log(error);
     }
